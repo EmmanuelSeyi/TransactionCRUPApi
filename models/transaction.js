a80716261de5e67671dc3f0d5521cfe46d464023
Arinze <38760901+EmmanuelSeyi@users.noreply.github.com>
2020-06-18 21:12:08 +0100

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,16 +1,53 @@
-const mongoose = require('mongoose')
-const transactionSchema = mongoose.Schema({
-    amount: {
-        type: String,
-        required: true
-    },
-    transactionDate: {
-        type: Date,
-        required: true,
-    },
-    senderName: {
-        type: String,
-        required: true
-    }
+const mongoose = require("mongoose");
+
+
+
+const transactionSchema = new mongoose.Schema({
+
+    date: {
+
+            type: Date,
+
+            default: Date.now()
+
+        },
+
+        from: {
+
+            type: String,
+
+            required: true
+
+        },
+
+        to: {
+
+            type: String,
+
+            required: true
+
+        },
+
+        description: {
+
+            type: String,
+
+            required: true
+
+        },
+
+        payment_method: {
+
+            type: String,
+
+            default: "Cash"
+
+        },
+
+        isCleared: Boolean
+
 })
-module.exports = mongoose.model('Transaction', transactionSchema)
+
+
+
+module.exports = mongoose.model("transaction", transactionSchema);
